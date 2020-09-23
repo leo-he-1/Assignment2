6ebdf1cdde032eb11efb506b6ff74e3d57003d54
leo-he-1 <tuh28889@temple.edu>
2020-09-23 14:30:43 -0400

    Renamed the main activity

--- app/src/main/java/com/example/assignment2/FormActivity.java
+++ app/src/main/java/com/example/assignment2/FormActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class FormActivity extends AppCompatActivity {
     Button save;
     EditText name, email, password, confirmpassword;
     @Override
@@ -27,15 +27,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //this will check if any of the edit texts are empty
                 if (name.getText().toString().isEmpty() || email.getText().toString().isEmpty() || password.getText().toString().isEmpty() || confirmpassword.getText().toString().isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please enter all of the information.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(FormActivity.this, "Please enter all of the information.", Toast.LENGTH_SHORT).show();
                 }
                 //checks if the passwords match
                 if(password.getText().toString().equals(confirmpassword.getText().toString())){
                     //if everything is ok then it will display the user's name
-                    Toast.makeText(MainActivity.this, name.getText().toString(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(FormActivity.this, name.getText().toString(), Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    Toast.makeText(MainActivity.this, "Passwords do not match.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(FormActivity.this, "Passwords do not match.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
